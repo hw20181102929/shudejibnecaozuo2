c71d74227d413ae6b7f695b893c1e9abf4da0473
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-11-15 08:33:08 +0800

    Initial Commit

---
+++ shudejibnecaozuo2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  shudejibnecaozuo2
+//
+//  Created by s20181102929 on 2019/11/15.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
